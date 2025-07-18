83a587c37c5a4d1c4db1fc57d777ec0cab30f170
raaidmustafa <raaidmustafa159@gmail.com>
2025-07-18 13:22:22 +0500

    functions

--- functions.js
+++ functions.js
@@ -1,4 +1,40 @@
-
-function sum(a , b) {
-    alert("the sum is: /t " + (a + b))
+function vowels(str) {
+    let count = 0;
+    for(const char of str.toLowerCase()) {
+        if (
+            char === 'a' || 
+            char === 'e' || 
+            char === 'i' || 
+            char === 'o' || 
+            char === 'u'
+        ) {
+            count++;
+        }
+        
+     }
+  
+  console.log(count);
 }
+
+const countvowels = (str) => {
+    let count = 0;
+    for (const char of str.toLowerCase()) {
+        if (
+            char === 'a' || 
+            char === 'e' || 
+            char === 'i' || 
+            char === 'o' || 
+            char === 'u'
+        ) {
+            count++;
+        }
+    }
+    alert(count);
+}
+
+let nums = [67, 52, 39];
+
+let calcSquare = (num) => {
+    console.log(num * num);
+};
+nums.forEach(calcSquare);
