c4ca81cb061afeaf2af70a57eccbf10dd6fea376
raaidmustafa <raaidmustafa159@gmail.com>
2025-07-18 16:14:10 +0500

    functions

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+/*
 function vowels(str) {
     let count = 0;
     for(const char of str.toLowerCase()) {
@@ -38,3 +39,36 @@ let calcSquare = (num) => {
     console.log(num * num);
 };
 nums.forEach(calcSquare);
+
+
+const multiply = (a, b) => {
+    alert(a * b);
+};
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let oddArr = arr.filter((val) => {
+    return val % 2 !== 0;
+});
+
+let evenArr = arr.filter((val) => {
+    return val % 2 === 0;
+});
+alert("Even numbers are: " + evenArr);
+alert("Odd numbers are: " + oddArr);
+
+let no = [1, 2, 3, 4, 5, 6];
+no.forEach((num) => {
+    console.log(num * num);
+});
+
+
+let num = [67, 52, 39];
+let squares = num.map(n => n * n);
+alert("Square of numbers are: " + squares.join(", "));*/
+
+
+
+let nums = [67, 52, 39];
+nums.map((num) => {
+    alert(num);
+});
