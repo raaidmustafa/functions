9e46be439979bbd19c3aebb012fce74c56218383
raaidmustafa <raaidmustafa159@gmail.com>
2025-07-21 11:50:59 +0500

    latest

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerText = h2.innerText + " by Raaid Mustafa";  
+
+let div = document.querySelector("div");
+console.dir(div.innerText);
+
